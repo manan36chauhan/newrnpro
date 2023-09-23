1b157584343ba1791107e2821ebc020ca485ca09
manan36chauhan <cmanan36@gmail.com>
2023-09-23 12:08:40 -0700

    notification update

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Background from './Background'
 
-export default function home() {
+export default function Home() {
   return (
     <Background>
 
